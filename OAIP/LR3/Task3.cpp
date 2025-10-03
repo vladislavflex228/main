6472916b71cb0ebbf8e78461e073a7256767ffa2
vladislavflex228 <vladislavflex228.com>
2025-10-04 00:16:21 +0300

    Сделал Task3 LR3

---
+++ OAIP/LR3/Task3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#define PI 3.141592653
+#include<cmath>
+
+int main()
+{
+    std::cout<<"Задание 3\nВыполнял Антонюк Владислав Группа 553501\n";
+    std::cout<<"Программа находит значение функции y = sin(x) - cos(x) на промежутке [A; B], где A = 0, B = pi/2, ";
+    std::cout<<"с шагом H = (B-A)/M, где M = 20, используя цикл for\n";
+    long double M = 20;
+    long double A = 0;
+    long double B = PI / 2;
+    long double H = (B - A) / M;
+    for (int i = 0; A + 1. * i * H <= B; i++)
+    {
+        long double x = A + 1. * i * H;
+        long double func = std::sin(x) - std::cos(x);
+        std::cout<<"При Xi = A + H * i, где i = " << i<< ", y =  " << func << std::endl;
+    }
+
+
+    return 0;
+}
