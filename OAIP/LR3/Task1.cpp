18cc5a6ca79d963e5d6e43389d19224be2df5d79
vladislavflex228 <vladislavflex228.com>
2025-10-03 23:48:00 +0300

    Сделал Task1 LR3

--- OAIP/LR3/Task1.cpp
+++ OAIP/LR3/Task1.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
 int main()
 {
-std::cout<<"Всем привет";
-    return 0;
+
+    std::cout<<"1 Задание\nВыполнил Антонюк Владислав Группа 553501\n";
+    std::cout<<"Находится сумма квадратов разности первых 30 элементов (Ai-Bi)*2, где:\n";
+    std::cout<<"Ai=i, если i - четное, Ai=i/2, если нечетное\n";
+    std::cout<<"Bi=i^2, если i - четное, Bi=i^3 -- нечетное\n";
+    
+
+
+    double Sum = 0;
+    int i = 1;
+    while (i <= 30) {
+        double x = (i % 2) ? i : (i / 2);
+        double y = (i % 2) ? i * i : i * i * i;
+        Sum += (x - y) * (x - y);
+        i++;
+    }
+    std::cout << "Сумма равна " << Sum << std::endl;
+    
+    
+
+
+
+
+
+
 }
