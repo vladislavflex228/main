a8ab1edf5e2fdb3a496ca17760a8629a9c4000a7
vladislavflex228 <vladislavflex228.com>
2025-10-05 01:06:59 +0300

    Сделал Task6

---
+++ OAIP/LR3/Task6.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include"check.h"
+#include"func.h"
+
+int main()
+{
+    std::cout << "Задание 6 \n Выполнил Антонюк Владислав Группа 553501\n";
+
+    std::cout<<"Вычислить значения функций sin(x),cos(x),ln(|x|) в введенном значении х т найти наименьший из них\n";
+
+    std::cout<<"Для начала выберем x \n";
+    double x;
+    checkDouble0(x);
+
+    double a = sin(x);
+    double b = cos(x);
+    double c = log(abs(x));
+
+    double minVal = a;
+    if (b < minVal) 
+    {
+        minVal = b;
+    }
+    else if(c < minVal) 
+    {
+        minVal = c;
+    }
+
+    std::cout << "Минимальное значение: " << minVal << std::endl;
+
+
+
+
+    return 0;
+}
